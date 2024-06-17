d87296e4ef2239d064dff3b50129f61249a0382e
VoxanyNet <contact@voxany.net>
2024-06-16 22:33:00 -0400

    move translate coordinates function to game library

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use macroquad::{math::Vec2, window::screen_height};
+
 pub mod timeline;
 pub mod proxies;
 pub mod time;
@@ -13,4 +15,12 @@ pub fn uuid() -> String {
     // AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
     macroquad::rand::srand(SystemTime::now().duration_since(UNIX_EPOCH).expect("we went back in time!").as_nanos() as u64);
     macroquad::rand::rand().to_string()
+}
+
+pub fn translate_coordinates(macroquad_coords: &Vec2) -> Vec2 {
+    // translate macroquad coords to rapier coords
+    Vec2 { 
+        x: macroquad_coords.x, 
+        y: (macroquad_coords.y * -1.) + screen_height()
+    }
 }
