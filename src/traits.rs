42f23c7ddd92f9d027b94935980cdb816749b963
VoxanyNet <contact@voxany.net>
2024-08-07 02:08:20 -0400

    removed unused traits

--- src/traits.rs
+++ src/traits.rs
@@ -19,175 +19,13 @@ use rapier2d::prelude::RigidBodyPosition;
 use crate::space::Space;
 use crate::{macroquad_to_rapier, rapier_mouse_world_pos, rapier_to_macroquad};
 
-pub trait Velocity {
-    fn get_velocity(&self) -> Vec2;
-    fn set_velocity(&mut self, velocity: Vec2);
-}
-
-pub trait Damagable {
-    fn damage(&mut self, damage: i32) {
-        self.set_health(
-            self.get_health() - damage
-        )
-    }
-
-    fn get_health(&self) -> i32;
-    fn set_health(&mut self, health: i32);
-}
-
-pub trait Breakable: Damagable + HasRect {
-    fn highlight(&mut self) {
-
-        let mouse_pos = Vec2::new(
-            macroquad::input::mouse_position().0,
-            macroquad::input::mouse_position().1
-        );
-
-        if self.get_rect().contains(mouse_pos) {
-            self.set_highlighted(true);
-        }
-
-        else {
-            self.set_highlighted(false);
-        }
-    }
-
-    fn get_highlighted(&self) -> bool;
-    fn set_highlighted(&mut self, highlighted: bool);
-}
-
-pub trait Collidable: HasRect + Velocity {
-
-    fn collide(&mut self, collider: &mut dyn Collidable, dt: TimeDelta) {
-
-        // check where our rect will be when it next moves
-        let mut next_rect = self.get_rect().clone();
-
-        next_rect.x += self.get_velocity().x * dt.num_milliseconds() as f32;
-        next_rect.y += self.get_velocity().y * dt.num_milliseconds() as f32;
-
-        if collider.get_rect().overlaps(&mut next_rect) {
-            
-            // add our velocity to the collider
-            collider.set_velocity(
-                collider.get_velocity() + self.get_velocity()
-            );
-
-            // invert current velocity
-            self.set_velocity(
-                self.get_velocity() * -0.05
-            );
-
-        }
-
-    }
-}
-
-fn round(x: f32) -> f32 {
-    f32::trunc(x  * 10.0) / 10.0 // or f32::trunc
-}
-
-pub trait Friction: HasRect + Velocity {
-    fn apply_friction(&mut self, dt: TimeDelta) {
-
-        self.set_velocity(
-            self.get_velocity() + ((-self.get_velocity() * self.friction_coefficient()) * (dt.num_milliseconds() as f32 / 1000.))
-        );
-
-        self.set_velocity(
-            Vec2::new(round(self.get_velocity().x), round(self.get_velocity().y))
-        );
-    }
-
-    fn friction_coefficient(&self) -> f32;
-}
-
-pub trait Controllable: HasRect + Velocity {
-    fn control(&mut self, dt: TimeDelta) {
-
-        let mut velocity = self.get_velocity();
-        let acceleration = self.get_acceleration();
-
-        if macroquad::input::is_key_down(self.right_bind().into()) {
-            velocity.x += acceleration * dt.num_milliseconds() as f32;
-        }
-
-        if macroquad::input::is_key_down(self.left_bind().into()) {
-            velocity.x -= acceleration * dt.num_milliseconds() as f32
-
-        }
-
-        if macroquad::input::is_key_down(self.up_bind().into()) {
-            velocity.y -= acceleration * dt.num_milliseconds() as f32
-        }
-
-        if macroquad::input::is_key_down(self.down_bind().into()) {
-            velocity.y += acceleration * dt.num_milliseconds() as f32
-        }
-
-        // update to the final velocity
-        self.set_velocity(
-            velocity
-        );
-
-    }
-
-    fn get_acceleration(&self) -> f32;
-    fn set_acceleration(&mut self, acceleration: f32);
-
-    fn up_bind(&mut self) -> KeyCode;
-    fn down_bind(&mut self) -> KeyCode;
-    fn left_bind(&mut self) -> KeyCode;
-    fn right_bind(&mut self) -> KeyCode;
-}
-
-pub trait Moveable: HasRect + Velocity {
-    fn move_by_velocity(&mut self, dt: TimeDelta) {
-
-        let mut rect = self.get_rect();
-
-        //println!("{}", self.get_velocity().x * (dt.num_milliseconds() as f32 / 1000.));
-
-        rect.x += self.get_velocity().x * (dt.num_milliseconds() as f32 / 1000.);
-        rect.y += self.get_velocity().y * (dt.num_milliseconds() as f32 / 1000.);
-
-        self.set_rect(rect);
-    }
-}
-pub trait HasRect {
-    fn get_rect(&self) -> Rect;
-    fn set_rect(&mut self, rect: Rect);
-}
-
 // pub trait HasCollider {
 //     fn get_collider(&self) -> Collider;
 //     fn set_collider(&mut self, collider: Collider);
 // }
 
 
-
-pub trait Color {
-    fn color(&mut self) -> &mut macroquad::color::Color;
-}
-
-pub trait Drawable: HasRect + Color {
-    fn draw(&mut self, camera_offset: &Vec2) {
-        macroquad::shapes::draw_rectangle(self.get_rect().x, self.get_rect().y, self.get_rect().w, self.get_rect().h, *self.color());
-    }
-}
-
-pub struct ResizeHandles {
-    top_left: Rect,
-    top_right: Rect,
-    bottom_left: Rect,
-    bottom_right: Rect
-}
-
-pub trait HasRigidBody {
-    fn get_rigid_body_handle(&self) -> &RigidBodyHandle;
-}
-
-pub trait HasCollider: Color {
+pub trait HasCollider {
     fn get_collider_handle(&self) -> &ColliderHandle;
     fn get_selected(&mut self) -> &mut bool;
     fn get_dragging(&mut self) -> &mut bool; // structure is currently being dragged
@@ -239,7 +77,7 @@ pub trait HasCollider: Color {
 
         let drag_offset = self.get_drag_offset().unwrap(); // there shouldn't be a situation where get_dragging returns true and there is no drag offset
         
-        let mut collider = space.collider_set.get_mut(*self.get_collider_handle()).unwrap();
+        let collider = space.collider_set.get_mut(*self.get_collider_handle()).unwrap();
 
         let mouse_pos = rapier_mouse_world_pos(camera_rect);
 
@@ -269,14 +107,6 @@ pub trait HasCollider: Color {
 
     fn update_is_dragging(&mut self, space: &mut Space, camera_rect: &Rect) {
         // Determine if the collider is being dragged
-
-        if *self.get_dragging() {
-            *self.color() = GREEN.into();
-        }
-
-        else {
-            *self.color() = RED.into();
-        }
 
         if !*self.get_selected() {
             *self.get_dragging() = false;
@@ -345,7 +175,7 @@ pub trait HasCollider: Color {
 
     }
 
-    async fn draw(&mut self, camera_offset: &Vec2, space: &Space) {
+    async fn draw_collider(&mut self, space: &Space) {
         let collider_handle = self.get_collider_handle();
         let collider = space.collider_set.get(*collider_handle).expect("Invalid collider handle");
 
@@ -388,7 +218,7 @@ pub trait HasCollider: Color {
             ((position.translation.y) * -1.) + screen_height(), 
             hx * 2., 
             hy * 2., 
-            DrawRectangleParams { offset: macroquad::math::Vec2::new(0.5, 0.5), rotation: rotation.angle() * -1., color: *self.color() }
+            DrawRectangleParams { offset: macroquad::math::Vec2::new(0.5, 0.5), rotation: rotation.angle() * -1., color: WHITE }
         );
 
         // for resize_handle in self.get_resize_handles() {
@@ -404,71 +234,5 @@ pub trait HasCollider: Color {
         
 
     }
-}
-
-pub trait HasOwner {
-    fn get_owner(&self) -> String;
-
-    fn set_owner(&mut self, uuid: String);
-}
-
-pub trait Scale {
-    fn get_scale(&self) -> u32;
-}
-
-pub trait Draggable: HasRect + Velocity {
-    fn drag(&mut self) {
-
-        if input::is_mouse_button_down(input::MouseButton::Left) & self.get_rect().contains(Vec2{x: input::mouse_position().0, y: input::mouse_position().1}) {
-            self.set_dragging(true)
-        }
-
-        if input::is_mouse_button_released(input::MouseButton::Left) {
-            self.set_dragging(false)
-        }
-
-        if !self.get_dragging() {
-            return;
-        }
-
-        let mouse_pos = Vec2{x: macroquad::input::mouse_position().0, y: macroquad::input::mouse_position().1};
-
-        let rect = self.get_rect();
-
-        let distance_to_mouse = Vec2::new(
-            mouse_pos.x - rect.x,
-            mouse_pos.y - rect.y
-        );
-        
-        self.set_velocity(
-            distance_to_mouse.normalize() * 1000.
-        );
-
-    }
-
-    fn get_dragging(&self) -> bool;
-
-    fn set_dragging(&mut self, dragging: bool);
-
-}
-
-pub trait Sound {
-    async fn play_sound(&self, sounds: &mut HashMap<String, macroquad::audio::Sound>) {
-        // load texture if not already
-        if !sounds.contains_key(&self.get_sound_path()) {
-            let sound = load_sound(&self.get_sound_path()).await.unwrap();
-
-            sounds.insert(self.get_sound_path(), sound);
-        }
-
-        let sound = sounds.get(&self.get_sound_path()).unwrap();
-
-        audio::play_sound_once(sound);
-    }
-
-    fn get_sound_path(&self) -> String;
-
-    fn set_sound_path(&mut self, sound_path: String);
-
 }
 
