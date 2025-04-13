065cca0eca36c9b7ec9c9e93fac4898c38bead21
Voxany <contact@voxany.net>
2025-04-13 00:02:52 -0400

    get texture blocking

--- src/texture_loader.rs
+++ src/texture_loader.rs
@@ -1,3 +1,4 @@
+use futures::executor::block_on;
 use fxhash::FxHashMap;
 use macroquad::texture::{self, load_texture, Texture2D};
 
@@ -21,6 +22,23 @@ impl TextureLoader {
         if !self.cache.contains_key(texture_path) {
 
             let texture = load_texture(&texture_path).await.unwrap();
+            
+            texture.set_filter(texture::FilterMode::Nearest);
+
+            self.cache.insert(texture_path.clone(), texture);
+
+        }
+
+        self.cache.get(texture_path).unwrap()
+    }
+
+    pub fn get_blocking(&mut self, texture_path: &String) -> &Texture2D {
+        // this can probably be optimized with a match statement but i cant figure it out the borrowing stuff
+        if !self.cache.contains_key(texture_path) {
+
+            let texture = block_on(
+                load_texture(&texture_path)
+            ).unwrap();
             
             texture.set_filter(texture::FilterMode::Nearest);
 
